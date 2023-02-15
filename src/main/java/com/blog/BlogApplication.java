fbee01655289bcba9e929c16ac0fd87ba79dde55
Aditi Bokil <bokil.aditi@gmail.com>
2023-02-15 17:41:13 +0530

    Blog Application

---
+++ src/main/java/com/blog/BlogApplication.java
@@ -0,0 +1,20 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+
+}
---
+++ src/main/java/com/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package com.blog.services.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blog.entities.User;
+import com.blog.exceptions.ResourceNotFoundException;
+import com.blog.payloads.UserDto;
+import com.blog.repositories.*;
+import com.blog.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		User user = this.modelMapper.map(userDto, User.class);
+		User savedUser = this.userRepo.save(user);
+
+		return this.modelMapper.map(savedUser, UserDto.class);
+		
+	}
+
+	@Override
+	public UserDto updateUser(UserDto userDto, Integer userId) {
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", " Id ", userId));
+
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setPassword(userDto.getPassword());
+		user.setAbout(userDto.getAbout());
+
+		User updatedUser = this.userRepo.save(user);
+		return this.modelMapper.map(updatedUser, UserDto.class);
+		
+	}
+
+	@Override
+	public UserDto getUserById(Integer userId) {
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", " Id ", userId));
+
+		return this.modelMapper.map(user, UserDto.class);
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		List<User> users = this.userRepo.findAll();
+		List<UserDto> userDtos = users.stream().map((user) -> this.modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
+		return userDtos;
+	}
+
+	@Override
+	public void deleteUser(Integer userId) {
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User", "Id", userId));
+		this.userRepo.delete(user);
+		
+	}
+
+	
+}
